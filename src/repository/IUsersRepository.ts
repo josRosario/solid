67a75d78e579c33958ff300a93de0c327e704fb9
josRosario <jrosario@intellisysdcorp.com>
2024-06-04 15:10:34 -0400

    liskov principle

--- src/repository/IUsersRepository.ts
+++ src/repository/IUsersRepository.ts
@@ -3,5 +3,4 @@ import { User } from "../entities/User";
 export interface IUserRepository{
     findByEmail(email:string):Promise<User | null>;
     save(user:User):Promise<void>;
-    encryptPassword(password:string):Promise<User | null>
 }
